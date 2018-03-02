e0add001268132be4b883bccf2d2f3ecb6d33051
Joshua Westerheide <dev@jdoubleu.de>
2018-03-02 17:57:29 +0100

    Add license file and add as file header

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Column Layout',
     'description' => 'Adds column configuration to fluid styled content elements',
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3_MODE') || die();
 
 call_user_func(function ($extKey) {
