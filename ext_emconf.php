c7cf6ba36ba51dcac971d092e8972f4c834a3bd0
Joshua Westerheide <dev@jdoubleu.de>
2018-03-02 17:50:53 +0100

    Initialize typo3 extension

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Column Layout',
+    'description' => 'Adds column configuration to fluid styled content elements',
+    'category' => 'fe',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99'
+        ]
+    ],
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => true,
+    'author' => 'Arndtteunissen',
+    'author_email' => 'dev@arndtteunissen.de',
+    'author_company' => 'Arndtteunissen'
+];
