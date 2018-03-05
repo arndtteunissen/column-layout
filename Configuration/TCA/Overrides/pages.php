ca4d6dd4727d14360a93cdd67a3243fa68683176
Joshua Westerheide <dev@jdoubleu.de>
2018-03-05 11:11:41 +0100

    Setup typo3 extension for backend

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile('column_layout', 'Configuration/TSConfig/gridsystem.tsconfig', 'Column Layout Gridsystem');
---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('column_layout', 'Configuration/TypoScript', 'Main Setup');
