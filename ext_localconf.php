edefc51db0305c0fbafb5a2d5182a50ae56cdf62
Joshua Westerheide <dev@jdoubleu.de>
2018-03-05 17:45:27 +0100

    Implement hook to change the flexform data structure
    The data structure or flexform file depends on the current gridsystem, configured in the extension configuration.

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,5 +10,6 @@
 defined('TYPO3_MODE') || die();
 
 call_user_func(function ($extKey) {
-    
+    // FlexForm hook for the columns configuration field
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \Arndtteunissen\ColumnLayout\Hook\ColumnConfigurationGridsystemFlexFormHook::class;
 }, $_EXTKEY);
