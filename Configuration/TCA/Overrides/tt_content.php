236ecb6932c863704e7705167d9e4249843fa35a
Joshua Westerheide <dev@jdoubleu.de>
2018-03-05 17:19:44 +0100

    Add column configuration field to tt_content

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') || die();
+
+$ll = 'LLL:EXT:column_layout/Resources/Private/Language/locallang_db.xlf:';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
+    'tx_column_layout_column_config' => [
+        'label' => $ll . 'tt_content.tx_column_layout_column_config.label',
+        'config' => [
+            'type' => 'flex',
+            'ds' => []
+        ]
+    ]
+]);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;' . $ll . 'tt_content.tabs.tx_column_layout_column_config,tx_column_layout_column_config',
+    '',
+    'after:categories'
+);
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Extend table 'tt_content'
+#
+CREATE TABLE tt_content (
+    tx_column_layout_column_config TEXT
+);
