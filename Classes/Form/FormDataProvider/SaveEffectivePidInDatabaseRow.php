7d1955efa5585570da721cfd2f31f5ec8542a5ed
Joshua Westerheide <dev@jdoubleu.de>
2018-03-16 11:54:36 +0100

    Implement form data processor to save a new record's initial pid
    Before it will be overwritten in \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew::setPid.
    See also 619154216dd21e8c5cec66feb06f36e8da39062b.

---
+++ Classes/Form/FormDataProvider/SaveEffectivePidInDatabaseRow.php
@@ -0,0 +1,40 @@
+<?php
+namespace Arndtteunissen\ColumnLayout\Form\FormDataProvider;
+
+/*
+ * This file is part of the package arndtteunissen/column-layout.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew;
+use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+
+/**
+ * Saves the effectivePid from the (new) record in the database record
+ *
+ * @see DatabaseRowInitializeNew::setPid()
+ */
+class SaveEffectivePidInDatabaseRow implements FormDataProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function addData(array $result)
+    {
+        if ($result['command'] !== 'new') {
+            return $result;
+        }
+        if (!is_array($result['databaseRow'])) {
+            throw new \UnexpectedValueException(
+                'databaseRow of table ' . $result['tableName'] . ' is not an array',
+                1444431128
+            );
+        }
+
+        $result['databaseRow']['_tx_column_layout_orig_pid'] = $result['effectivePid'];
+
+        return $result;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -13,4 +13,13 @@ call_user_func(function ($extKey) {
     // Mod Web>Page layout customizations
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook'][] = \Arndtteunissen\ColumnLayout\Hook\LayoutPreviewHook::class . '->injectStyleSheet';
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawFooter'][] = \Arndtteunissen\ColumnLayout\Hook\LayoutPreviewHook::class;
+
+    // Extend FormData processing for flexforms as quickfix
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\Arndtteunissen\ColumnLayout\Form\FormDataProvider\SaveEffectivePidInDatabaseRow::class] = [
+        'depends' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseEffectivePid::class,
+            \TYPO3\CMS\Backend\Form\FormDataProvider\PageTsConfig::class
+        ]
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew::class]['depends'][] = \Arndtteunissen\ColumnLayout\Form\FormDataProvider\SaveEffectivePidInDatabaseRow::class;
 }, $_EXTKEY);
