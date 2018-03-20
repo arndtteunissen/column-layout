c1b6fed92582a3379fb1dcb75658a5751c942f7a
Joshua Westerheide <dev@jdoubleu.de>
2018-03-20 11:52:01 +0100

    Raise maximum required typo3 version to ^9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99'
+            'typo3' => '8.7.0-9.5.99'
         ]
     ],
     'state' => 'alpha',
