3a5a167165906b0788197fd2b0c3b3c7caef9df0
Tim Schreiner <schreiner@arndtteunissen.de>
2018-07-26 21:40:54 +0200

    Move custom configuration to arndtteunissen/column-layout-arndtteunissen package

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -12,4 +12,3 @@ defined('TYPO3_MODE') || die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('column_layout', 'Configuration/TypoScript', 'Main Setup');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('column_layout', 'Configuration/TypoScript/GridSystems/Foundation', 'Gridsystem :: Foundation');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('column_layout', 'Configuration/TypoScript/GridSystems/Bootstrap', 'Gridsystem :: Bootstrap');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('column_layout', 'Configuration/TypoScript/GridSystems/Custom', 'Gridsystem :: Custom');
