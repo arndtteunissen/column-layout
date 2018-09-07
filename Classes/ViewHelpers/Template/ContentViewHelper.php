23f986bfe73da927b45684954b9b13076575f78e
Joshua Westerheide <dev@jdoubleu.de>
2018-09-07 13:04:29 +0200

    Improve template ContentViewHelper
    This ViewHelper should only render its content in the best performant way.

--- Classes/ViewHelpers/Template/ContentViewHelper.php
+++ Classes/ViewHelpers/Template/ContentViewHelper.php
@@ -9,8 +9,8 @@ namespace Arndtteunissen\ColumnLayout\ViewHelpers\Template;
  */
 
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+use TYPO3Fluid\Fluid\Core\Compiler\TemplateCompiler;
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\ViewHelperNode;
 
 /**
  * This ViewHelper is used to mark content inside the grid templates.
@@ -18,18 +18,27 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
  */
 class ContentViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
     /**
      * @var bool
      */
     protected $escapeOutput = false;
 
     /**
+     * Default render method - simply calls renderStatic() with a
+     * prepared set of arguments.
+     *
+     * @return string Rendered string
+     */
+    public function render()
+    {
+        return $this->renderChildren();
+    }
+
+    /**
      * {@inheritdoc}
      */
-    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    public function compile($argumentsName, $closureName, &$initializationPhpCode, ViewHelperNode $node, TemplateCompiler $compiler)
     {
-        return $renderChildrenClosure();
+        return sprintf('%s()', $closureName);
     }
 }
