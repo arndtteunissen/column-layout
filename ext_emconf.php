fbb0665a01edffcfe5ada842302c850f3f551e8c
Fawad Mirzad <mirzad@arndtteunissen.de>
2021-10-07 12:42:42 +0200

    Change TYPO3 version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99'
+            'typo3' => '10.1.0-10.5.99'
         ]
     ],
     'state' => 'beta',
